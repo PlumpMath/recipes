2e07d650615e2e445a03a2b9f7f8ef75160002ae
Lucas Stadler <mi.au@papill0n.org>
2013-09-25 21:46:02 +0200

    Use clojure 1.5.1 (required for datomic).

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject recipes "0.0.x"
   :description "A sane API for recipes"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [korma "0.3.0-RC2"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [compojure "1.1.5"]
